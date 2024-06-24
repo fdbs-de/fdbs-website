e1f30df3f6d0503d02dc60a338c3a292d9047f6f
Alyx Freuwört <159260986+alyx-at-fdbs@users.noreply.github.com>
2024-06-24 16:28:59 +0200

    Add lodash.throttle Add pagination to media explorer Fine tune media explorer to include path in url

--- components/media/MediaExplorer.vue
+++ components/media/MediaExplorer.vue
@@ -2,7 +2,7 @@
     <div class="explorer">
         <div class="header py-4">
             <IodIconButton type="button" variant="text" icon="refresh" @click="fetch" background="var(--color-text-soft)" v-tooltip="'Aktualisieren'"/>
-            <IodInput class="!h-10 w-64" placeholder="Suchen"/>
+            <IodInput class="!h-10 w-64" placeholder="Suchen" clearable v-model="search"/>
             <HeSpacer />
             <IodButtonGroup class="bg-zinc-100">
                 <IodIconButton type="button" icon="grid_view" background="var(--color-text-soft)" :variant="layout === 'grid' ? 'contained' : 'text'" @click="layout = 'grid'"/>
@@ -18,73 +18,134 @@
 
         <div class="main small-scrollbar">
             <div class="grid" v-if="items.length && layout === 'grid'">
-                <MediaItem v-for="item in items" :item="item" @click="navigateOrDownload($event, item)" />
+                <MediaItem v-for="item in items" :item="item" @click="navigateOrDownload(item)" />
             </div>
 
             <div class="list" v-if="items.length && layout === 'list'">
-                <MediaRow is="button" v-for="item in items" :item="item" @click="navigateOrDownload($event, item)" />
+                <MediaRow is="button" v-for="item in items" :item="item" @click="navigateOrDownload(item)" />
             </div>
         </div>
 
         <div class="footer">
-            <IodPagination v-if="items.length" :total="items.length" @set-page="path = $event"/>
+            <IodPagination v-model="pagination"/>
             <HeSpacer />
-            <!-- <IodSelect class="!h-10" v-tooltip="'Einträge pro Seite'" :modelValue="20" @update:modelValue="setPagination({ size: parseInt($event) })" :options="[
+            <IodSelect class="!h-10" v-tooltip="'Einträge pro Seite'" :modelValue="pagination.size" @update:modelValue="setPagination({ size: parseInt($event) })" :options="[
                 { value: 10, text: '10 pro Seite' },
                 { value: 20, text: '20 pro Seite' },
                 { value: 50, text: '50 pro Seite' },
                 { value: 100, text: '100 pro Seite' },
                 { value: 250, text: '250 pro Seite' },
                 { value: 100000000, text: 'Alle' },
-            ]"/> -->
+            ]"/>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+    import type { MediaItem } from '~/types/media'
+    import throttle from 'lodash.throttle'
+
+    type Pagination = {
+        from: number,
+        to: number,
+        total: number,
+        page: number,
+        lastPage: number,
+        size: number,
+    }
+
+
+    
     const props = defineProps({
         root: {
             type: String,
             default: '',
-        }
+        },
+        layout: {
+            type: String,
+            default: 'grid',
+        },
     })
 
-    const path = ref(props.root)
-    const items = ref([])
+
+    
+    const route = useRoute()
     const loading = ref(false)
-    const layout = ref('grid')
+    const path = ref(route.query?.path || props.root)
+    const items = ref([])
+    
+    const layout = ref(props.layout)
+    const search = ref('')
+    const pagination = ref({
+        size: 20,
+        from: 0,
+        to: 0,
+        page: 1,
+        lastPage: 1,
+        total: 0,
+    })
 
-    async function fetch()
-    {
+
+
+    async function fetch() {
         loading.value = true
-        const {data} = await useAxios().get('/media/'+path.value)
+
+        try {
+            const {data} = await useAxios().get(apiRoute('/media/:path', {
+                path: path.value,
+                size: pagination.value.size,
+                page: pagination.value.page,
+                filter_search: search.value,
+            }))
+
+            pagination.value.size = data.meta.per_page
+            pagination.value.from = data.meta.from
+            pagination.value.to = data.meta.to
+            pagination.value.page = data.meta.current_page
+            pagination.value.lastPage = data.meta.last_page
+            pagination.value.total = data.meta.total
+    
+            items.value = data.data
+        }
+        catch (error)
+        {}
+
         loading.value = false
+    }
+    const throttledFetch = throttle(fetch, 300)
 
-        items.value = data.data
+    function setPagination(value: Partial<Pagination>) {
+        pagination.value = { ...pagination.value, ...value }
     }
 
-    function navigateOrDownload(e: MouseEvent, item: any)
-    {
-        if (item.mime_type === 'directory')
-            openDirectory(item.src_path)
-        else
-            downloadFile(item.cdn_path)
+
+
+    function navigateOrDownload(item: MediaItem) {
+        item.mime_type === 'directory' ? openDirectory(item.src_path as string) : downloadFile(item.cdn_path as string)
     }
 
-    function downloadFile(p: string)
-    {
-        // open in new tab
+    function downloadFile(p: string) {
         window.open(p, '_blank')
     }
 
-    function openDirectory(p: string)
-    {
+    function openDirectory(p: string) {
+        // Early return if above root
         if (!p.startsWith(props.root)) return
 
+        // Early return if already open
+        if (p === path.value) return
+        
+        // Set path
         path.value = p
+
+        // Push state
+        navigateTo(`?path=${path.value}`)
     }
 
-    watch(path, fetch, { immediate: true, deep: true })
+
+
+    useRouter().beforeEach((to) => openDirectory(to.query.path as string || props.root))
+    watch(() => [path, search, pagination.value.size, pagination.value.page], throttledFetch, { deep: true })
 </script>
 
 <style lang="sass" scoped>
--- pages/intern/spezifikationen.vue
+++ pages/intern/spezifikationen.vue
@@ -1,7 +1,7 @@
 <template>
     <SectionHero title="Spezifikations Datenbank" image="/images/content/banner/foodservice_365702048_356901877_454625888.webp" />
     <SectionText size="page">
-        <MediaExplorer root="domain/spezifikationen" />
+        <MediaExplorer root="domain/spezifikationen" layout="list" />
     </SectionText>
 </template>
 
