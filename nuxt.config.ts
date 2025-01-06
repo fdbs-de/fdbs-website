d2f0c9e6b1d23a43ef63b0d44d56af0b309f8368
Melina Kimmling <166021608+melina-at-fdbs@users.noreply.github.com>
2025-01-06 08:36:20 +0100

    Integrate PostHog Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,8 @@ export default defineNuxtConfig({
           frontendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.FRONTEND_DOMAIN ?? 'localhost:3000'}`,
           websiteUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.WEBSITE_DOMAIN ?? 'localhost:3001'}`,
           screensUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.SCREENS_DOMAIN ?? 'localhost:3002'}`,
+          posthogPublicKey: 'phc_IUjuGhQ1tgOqVVLtbrbnpy4yEYgS8CJJtNZUTIDYBUO',
+          posthogHost: 'https://eu.i.posthog.com',
       },
   },
 
