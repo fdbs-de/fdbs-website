2caa4d9f7e0f9277b505d4b6776696e8ffb5ed3b
Melina Kimmling <166021608+melina-at-fdbs@users.noreply.github.com>
2025-02-05 16:13:52 +0100

    add wiki article

---
+++ pages/intern/wiki/uebertragung-von-kontakten-ins-iphone.vue
@@ -0,0 +1,65 @@
+<template>
+    <SectionHero title="Übertragung von Kontakten ins iPhone"
+        image="/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/istockphoto-1307714091-612x612.jpg" />
+    <SectionText>
+        <HeDivider />
+        <span>
+            Kategorie: <b>IT</b><br>
+            Tags: <b>Kontakte, Smartphone</b>
+        </span>
+        <HeDivider />
+        <div class="formatted-content">
+            <p>Um alle Kontaktdaten aller Kollegen ins iPhone zu übertragen, ist folgende Methode notwendig. Dabei ist
+                zu beachten, dass diese die Kontaktdaten lediglich einmalig überträgt. </p>
+            <div icon="star" class="key-fact"><span class="icon" data-icon="star" contenteditable="false"></span>
+                <div class="content">
+                    <p>Bei Problemen oder Unklarheiten, wende dich gerne an die IT.</p>
+                </div>
+            </div>
+            <p></p>
+            <h2>Kontakte im Outlook importieren</h2>
+            <p>Im Outlook wird das allgemeine Adressbuch aufgerufen. Die Informationen darin werden systemseitig
+                automatisch gepflegt und sind zu 99% korrekt.</p>
+            <p></p><img class="content-image"
+                src="/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/contacts-1.png" alt="">
+            <p></p>
+            <p>Alle Kontakte, die auf dem iPhone ebenfalls zur Verfügung stellen sollen, werden nun markiert:</p><img
+                class="content-image" src="/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/contacts-2.png"
+                alt="">
+            <p></p>
+            <p>Die Besprechungsräume sowie Team-Postfächer sollten dabei nicht ausgewählt werden. Die Daten sind meist
+                irrelevant.</p><img class="content-image"
+                src="/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/contacts-3.png" alt="">
+            <p></p>
+            <p>Per Rechtsklick wird nun "Zu den Kontakten hinzufügen" ausgewählt:</p><img class="content-image"
+                src="/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/contacts-4.png" alt="">
+            <p></p>
+            <p>Nun sind die Kontakte bei den eigenen Outlook Kontakten zu finden:</p><img class="content-image"
+                src="/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/contacts-5.png" alt="">
+            <p></p>
+            <h2>Übertragen der Kontakte ins iPhone</h2>
+            <p>Die Übertragung von den eigenen Outlook Kontakten ins iPhone passiert automatisch. Der Vorgang könnte
+                allerdings 1-2 Minuten dauern. Anschließend tauchen die Kontakte bei allen Kontakten auf:</p><img
+                class="content-image" src="/images/content/wiki/IT/Aufbau einer VPN-Verbindung/Terminalserver_Suche.png"
+                alt="">
+        </div>
+    </SectionText>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    title: 'Übertragung von Kontakten ins iPhone',
+    description: 'IT, Kontakte, Smartphone',
+    category: 'IT',
+    image: '/images/content/wiki/IT/Uebertragung-von-Kontakten-ins-iPhone/istockphoto-1307714091-612x612.jpg',
+    middleware: ['auth', 'personal'],
+    layout: 'auth',
+})
+
+useSeoMeta({
+    title: 'Übertragung von Kontakten ins iPhone',
+    description: 'IT, Kontakte, Smartphone',
+})
+</script>
+
+<style lang="sass" scoped></style>
