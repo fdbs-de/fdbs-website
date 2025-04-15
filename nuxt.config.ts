df1554d21ef791c88f682eeaa4fb872bd58280e0
Alyx Freuwört <33777960+freuwoert@users.noreply.github.com>
2025-04-15 17:43:22 +0200

    Update asset structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,38 @@
+import tailwindcss from "@tailwindcss/vite"
+
 export default defineNuxtConfig({
-  vite: {
-      css: {
-          preprocessorOptions: {
-              sass: {
-                  api: 'modern',
-              },
-          },
-      },
-  },
+    css: [
+        '~/assets/css/app.css',
+    ],
 
-  css: [
-      '~/assets/css/app.sass',
-  ],
+    vite: {
+        plugins: [
+            tailwindcss(),
+        ],
+    },
 
-  modules: [
-      '@pinia/nuxt',
-      "@nuxtjs/tailwindcss",
-  ],
+    modules: [
+        '@pinia/nuxt',
+    ],
 
-  devtools: {
-      enabled: true,
-  },
+    pinia: {
+        storesDirs: ['./stores/**'],
+    },
 
-  pinia: {
-      storesDirs: ['./stores/**'],
-  },
+    runtimeConfig: {
+        public: {
+            backendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.BACKEND_DOMAIN ?? 'localhost:8000'}`,
+            frontendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.FRONTEND_DOMAIN ?? 'localhost:3000'}`,
+            websiteUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.WEBSITE_DOMAIN ?? 'localhost:3001'}`,
+            screensUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.SCREENS_DOMAIN ?? 'localhost:3002'}`,
+            posthogPublicKey: 'phc_IUjuGhQ1tgOqVVLtbrbnpy4yEYgS8CJJtNZUTIDYBUO',
+            posthogHost: 'https://eu.i.posthog.com',
+        },
+    },
 
-  runtimeConfig: {
-      public: {
-          backendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.BACKEND_DOMAIN ?? 'localhost:8000'}`,
-          frontendUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.FRONTEND_DOMAIN ?? 'localhost:3000'}`,
-          websiteUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.WEBSITE_DOMAIN ?? 'localhost:3001'}`,
-          screensUrl: `${process.env.BASE_PROTOCOL ?? 'http://'}${process.env.SCREENS_DOMAIN ?? 'localhost:3002'}`,
-          posthogPublicKey: 'phc_IUjuGhQ1tgOqVVLtbrbnpy4yEYgS8CJJtNZUTIDYBUO',
-          posthogHost: 'https://eu.i.posthog.com',
-      },
-  },
+    compatibilityDate: '2024-12-12',
 
-  compatibilityDate: '2024-12-12',
+    devtools: {
+        enabled: true,
+    },
 })
