d1d19b18df457c88d11491aa10eb1c13f976955b
Melina Kimmling <166021608+melina-at-fdbs@users.noreply.github.com>
2025-05-22 10:24:32 +0200

    Implement new wiki article

---
+++ pages/intern/wiki/whatsapp-business.vue
@@ -0,0 +1,111 @@
+<template>
+    <SectionHero title="WhatsApp Business" image="/images/content/wiki/generisch/fdbs_muster_grau.png" />
+    <SectionText>
+        <HeDivider />
+        <span>
+            Kategorie: <b>Marketing</b><br>
+            Tags: <b>Marketing, Kommunikation</b>
+        </span>
+        <HeDivider />
+        <div class="formatted-content">
+            <p></p>
+            <div icon="star" class="key-fact"><span class="icon" data-icon="star" contenteditable="false"></span>
+                <div class="content">
+                    <p><span style="color: rgb(104, 111, 111)">Bei Problemen oder Unklarheiten, wende dich gerne an die
+                            IT oder das Marketing.</span></p>
+                </div>
+            </div>
+            <p></p>
+            <p></p>
+            <h1>Einrichtung von WhatsApp Business</h1>
+            <p><span style="color: var(--color-text-soft)">Die App wird durch die IT autoamtisch auf das Handy gespielt.
+                    Es erscheint ein Pop-Up mit einem Hinweis. Danach wird die App installiert.</span></p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-1.png"
+                alt="">
+            <p><span style="color: var(--color-text-soft)">Melde dich in der App mit der Telefonnummer deines
+                    Diensthandys an. Falls WhatsApp Business schon mal bei dir instaliiert war, kannst du ein Backip
+                    wiederherstellen (Wenn nicht, klick auf "überspringen").</span></p>
+            <p><span style="color: var(--color-text-soft)">Synchronisiere deine Kontakte mit WhatsApp Business und
+                    erstelle ein Profil mit deinem Namen und dem FDBS-Profilbild von dir. Deinen Namen trägst du bei
+                    "Dein Unternehmensname" ein. Als Kategorie wählst du "Großhandel" oder "Fleischgroßhändler".</span>
+            </p>
+            <p><span style="color: var(--color-text-soft)">Dein Foto für das Profilbild findest du im vertriebseigenen
+                    SharePoint unter deinem Namen.</span>
+            </p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-2.png"
+                alt="">
+
+            <div icon="star" class="key-fact"><span class="icon" data-icon="star" contenteditable="false"></span>
+                <div class="content">
+                    <p><span style="color: rgb(104, 111, 111)">WhatsApp Business funktioniert grundsätzlich wie das
+                            herkömmliche WhatsApp. Zusätzlich bietet es jedoch einige nützliche Funktionen speziell für
+                            Unternehmen, wie Labels, Schnellantworten oder einen Produktkatalog. So wird die
+                            Kundenkommunikation effizienter, strukturierter und professioneller gestaltet.</span></p>
+                </div>
+            </div>
+            <p></p>
+
+            <h1>WhatsApp Business Tools</h1>
+            <h2>1. Katalog-Funktionen</h2>
+            <p>Hier können Produkte eingestellt werden. Füge optionale Details hinzu: Preis, Ktionspreis, Beschreibung,
+                Link zur Webseite, Produkt- oder Dienstleistungscode. Die Produkte sind dann für den Kunden über dein
+                Profil sichtbar.</p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-3.png"
+                alt="">
+            <p></p>
+
+            <h2>2. Labels</h2>
+            <p>Die Chats können mit verschiedenen Labels versehen werden, um schnell einen Überblick zu bekommen - vor
+                allem, wenn viele Gespräche parallel laufen. In der Label-Übersicht werden die Chats nach den
+                verschiedenen Zuordnungen aufgeteilt.</p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-4.png"
+                alt="">
+            <p></p>
+
+            <h2>3. Begrüßungs- und Abwesenheitsnachricht</h2>
+            <p>Du kannst automatische Nachrichten einstellen, welche der Kunde bekommt, wenn er dir zum ersten Mal
+                schreibt oder du gerade außer Dienst bzw. im Urlaub bist. Deine Arbeitszeiten richtest du in deinem
+                Profil unter "Geschäftszeiten" ein.</p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-5.png"
+                alt="">
+            <p></p>
+
+            <h2>4. Schnellantworten</h2>
+            <p>Hier können Textkürzel für häufig verwendete Nachrichten oder Informationen (angebote, Öffnungszeiten,
+                Adresse) erstellt werden. Um im Chat darauf zuzugreifen, rufst du sie z.B. mit "/Adresse" ab.</p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-6.png"
+                alt="">
+            <p></p>
+
+            <h2>5. Kontoverwaltung</h2>
+            <p>Du kannst hier deine Kontoinformationen bearbeiten.</p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-7.png"
+                alt="">
+            <p></p>
+
+            <h2>6. Kurzlink</h2>
+            <p>Du kannst hier deine Kontoinformationen bearbeiten.</p>
+            <img class="content-image" src="/images/content/wiki/marketing/whatsapp-business/whatsapp-business-8.png"
+                alt="">
+            <p></p>
+        </div>
+    </SectionText>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    title: 'WhatsApp Business',
+    description: 'Marketing, Kommunikation',
+    category: 'Marketing',
+    image: '/images/content/wiki/generisch/fdbs_muster_grau.png',
+    middleware: ['auth', 'personal'],
+    layout: 'auth',
+})
+
+useSeoMeta({
+    title: 'WhatsApp Business',
+    description: 'Marketing, Kommunikation',
+})
+</script>
+
+<style lang="sass" scoped></style>
