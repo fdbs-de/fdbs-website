50854781a6ba1d2253028e4632ad6522632c681e
Alyx Freuwört <alyx@freuwoert.com>
2025-07-28 18:47:54 +0200

    Add page content

---
+++ pages/eigenmarke-maxi-france.vue
@@ -0,0 +1,98 @@
+<template>
+    <SectionHero title="Eigenmarke: Maxi France" image="/images/content/banner/marken_509487561.webp" />
+    <SectionText>
+        <div class="mb-12 py-12 flex items-center justify-center bg-red-700 rounded-lg">
+            <img class="w-40" src="/images/content/maxi-france/mf_logo_2019.webp" alt="Maxi France">
+        </div>
+
+        <p>
+            Mit Maxi France erhalten Sie ein hochwertiges, französisches
+            Marken-Jungbullenfleisch. Die Marke ist leicht verständlich und lässt die Herkunft des Fleisches sofort
+            erkennen. Der Name ist für jedermann gut auszusprechen und vermittelt dem Endverbraucher französisches
+            Flair. Die drei Sterne im Logo stehen zudem für höchste Qualität. Dadurch bedingt entsteht ein maximaler
+            Wiedererkennungswert von Maxi France. Maxi France Erzeugnisse stammen zu 100 Prozent aus Frankreich – von
+            der Geburt der Tiere über die Aufzucht bis hin zur Schlachtung und Herstellung. Schon die Auswahl der
+            Lieferanten erfolgt nach strengen Vorgaben. Das Fleisch für die Maxi France Produkte kommt ausnahmslos von
+            vorab geprüften familiengeführten Bauernhöfen. Artgerechte Aufzucht, natürliche Haltungsbedingungen und
+            kurze Transportwege werden großgeschrieben. Maxi France unterliegt strengsten Kontrollen und sichert durch
+            ein umfassendes Qualitäts-Sicherungsprogramm lückenlose Rückverfolgbarkeit – vom landwirtschaftlichen
+            Betrieb bis zum POS. Dabei werden alle Prozesse von der Aufzucht bis zum POS unabhängig überwacht und
+            kontrolliert.
+        </p>
+
+        <img class="mb-12 rounded-lg" src="/images/content/maxi-france/mf_trennbild_1.webp" alt="Bullenherde auf einer Wiese">
+        <h2>Die Rassenvielfalt von Maxi France</h2>
+        <ul class="list-disc pl-6">
+            <li>
+                <strong>Salers – Fleischrasse</strong>
+            </li>
+            <li>
+                <strong>Charolais – Fleischrasse</strong>
+            </li>
+            <li>
+                <strong>Limousin – Fleischrasse</strong>
+            </li>
+            <li>
+                <strong>Blonde d‘Aquitaine – Fleischrasse</strong>
+            </li>
+            <li>
+                <strong>Race Normande – Zweinutzungsrasse</strong>
+            </li>
+            <li>
+                <strong>Montbéliarde – Zweinutzungsrasse</strong>
+            </li>
+        </ul>
+
+
+        <img class="mb-12 rounded-lg" src="/images/content/maxi-france/mf_trennbild_2.webp" alt="Bauer mit weißem Bullen auf einer Wiese">
+        <h2>Ihre Maxi-France-Vorteile:</h2>
+        <ul class="list-disc pl-6">
+            <li>100% pflanzliche Ernährung der Jungbullen, mineral- und vitaminhaltig</li>
+            <li>Ständige unabhängige Kontrollen</li>
+            <li>In Frankreich geborene, gehaltene und geschlachtete Jungbullen</li>
+            <li>Natürliche, artgerechte Aufzucht und Haltung der Rinder (Weide/offene Stallhaltung)</li>
+            <li>Kleinere Bauernhöfe/Erzeugergemeinschaften, traditionelle Landwirtschaft</li>
+            <li>Garantierte Rückverfolgbarkeit der Jungbullen von der Geburt bis zur Verkaufsstelle gemäß der Vorschriften</li>
+            <li>Garantierte Reifedauer für Barbecues und Bratenteilstücke</li>
+            <li>Ausgewählte Fleisch- und Zweinutzungsrassen, keine reinen Milchrassen</li>
+        </ul>
+        <br><br>
+
+
+
+        <h2>Maxi France – Unser Sortiment</h2>
+        <HeCarousel dots infinite arrows :interval="7500" overlay class="mb-12 w-full aspect-video rounded-lg">
+            <template #1><img src="/images/content/maxi-france/products/4990310_frz_jungbullen_semerrolle.webp" alt="Französisches Jungbullen Semerrolle" class="aspect-video object-contain"></template>
+            <template #2><img src="/images/content/maxi-france/products/4990311_frz_jungbullen_cote_de_boeuf.webp" alt="Französisches Jungbullen Côte de Boeuf" class="aspect-video object-contain"></template>
+            <template #3><img src="/images/content/maxi-france/products/4990313_frz_jungbullen_brust_mit_knochen_01.webp" alt="Französisches Jungbullen Brust mit Knochen 01" class="aspect-video object-contain"></template>
+            <template #4><img src="/images/content/maxi-france/products/4990313_frz_jungbullen_brust_mit_knochen_02.webp" alt="Französisches Jungbullen Brust mit Knochen 02" class="aspect-video object-contain"></template>
+            <template #5><img src="/images/content/maxi-france/products/4990314_frz_jungbullen_brust_mit_knochen.webp" alt="Französisches Jungbullen Brust mit Knochen" class="aspect-video object-contain"></template>
+            <template #6><img src="/images/content/maxi-france/products/4990315_frz_jungbullen_onglet.webp" alt="Französisches Jungbullen Onglet" class="aspect-video object-contain"></template>
+            <template #7><img src="/images/content/maxi-france/products/4990316_frz_jungbullen_inside_skirt.webp" alt="Französisches Jungbullen Inside Skirt" class="aspect-video object-contain"></template>
+            <template #8><img src="/images/content/maxi-france/products/4990317_frz_jungbullen_beef_hammer.webp" alt="Französisches Jungbullen Beef Hammer" class="aspect-video object-contain"></template>
+            <template #9><img src="/images/content/maxi-france/products/4990320_frz_jungbullen_flat_iron_steak.webp" alt="Französisches Jungbullen Flat Iron Steak" class="aspect-video object-contain"></template>
+            <template #10><img src="/images/content/maxi-france/products/4990330_frz_jungbullen_querrippe_01.webp" alt="Französisches Jungbullen Querrippe 01" class="aspect-video object-contain"></template>
+            <template #11><img src="/images/content/maxi-france/products/4990330_frz_jungbullen_querrippe_02.webp" alt="Französisches Jungbullen Querrippe 02" class="aspect-video object-contain"></template>
+            <template #12><img src="/images/content/maxi-france/products/4990330_frz_jungbullen_querrippe_03.webp" alt="Französisches Jungbullen Querrippe 03" class="aspect-video object-contain"></template>
+            <template #13><img src="/images/content/maxi-france/products/4990330_frz_jungbullen_querrippe_04.webp" alt="Französisches Jungbullen Querrippe 04" class="aspect-video object-contain"></template>
+            <template #14><img src="/images/content/maxi-france/products/24990314_frz_jungbullen_brust_ohne_knochen_02.webp" alt="Französisches Jungbullen Brust ohne Knochen 02" class="aspect-video object-contain"></template>
+            <template #15><img src="/images/content/maxi-france/products/24990314_frz_jungbullen_brust_ohne_knochen.webp" alt="Französisches Jungbullen Brust ohne Knochen" class="aspect-video object-contain"></template>
+        </HeCarousel>
+        
+        <div class="mb-12 p-6 flex flex-col gap-6 bg-red-700 rounded-lg">
+            <div>
+                <p class="m-0 text-red-100">Jetzt bestellen:</p>
+                <h3 class="m-0 text-red-50">0531 210 55 23</h3>
+            </div>
+        </div>
+    </SectionText>
+</template>
+
+<script lang="ts" setup>
+    useSeoMeta({
+        title: 'Maxi France – Eigenmarke',
+        description: 'Maxi France bietet hochwertiges, französisches Marken-Jungbullenfleisch mit garantierter Rückverfolgbarkeit und höchsten Qualitätsstandards.',
+    })
+</script>
+
+<style lang="sass" scoped></style>
