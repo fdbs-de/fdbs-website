6b9b5f1fa97dd0c3cc590a6230a6e2a79aff29c8
Melina Kimmling <166021608+melina-at-fdbs@users.noreply.github.com>
2025-09-09 07:59:20 +0200

    add job offer

---
+++ app/pages/karriere/elektromonteur-elektriker-als-servicetechniker.vue
@@ -0,0 +1,111 @@
+<template>
+    <SectionHero title="Elektromonteur / Elektriker als Servicetechniker"
+        image="/images/content/banner/karriere_513169496_482949832.webp" />
+    <SectionText>
+        <span>
+            Anstellungsart: <b>Vollzeit</b><br>
+            Tags: <b>Werkstatt & Kundendienst, Industrie</b>
+        </span>
+        <HeDivider />
+        <div class="formatted-content">
+            <p>FDBS ist ein Braunschweiger Großhandelsunternehmen und versteht sich seit über 100 Jahren als Partner für
+                Fleischereien, Bäckereien, Gastronomen, und Hoteliers. Wir liefern Frischeprodukte,
+                High-End-Convenience, sowie nahezu alles an Bedarfsartikeln und inspirieren unsere Kunden mit immer
+                neuen Trends.</p>
+            <p style="text-align: start">Unser Ziel ist es, unsere Kunden mit qualitativ hochwertigen Lebensmitteln
+                schnell und zuverlässig zu beliefern. Neben der Qualität und Herkunft unserer Produkte spielen eine hohe
+                Kundenorientierung sowie kooperative Beziehungen zu unseren Geschäftspartnern für uns eine große Rolle.
+            </p>
+            <p style="text-align: start">Wir sind ein sich ständig weiterentwickelndes Unternehmen mit über 100
+                Mitarbeitern und bieten ergänzend zu unserem großen Handelssortiment einen einzigartigen Rundum-Service
+                in Kundendienst, Werkstatt und Marketing, der den Anforderungen unserer Kunden entspricht.</p>
+            <p style="text-align: start">Du hast Interesse an der Food Branche, suchst Abwechslung und bist bereit
+                Verantwortung zu übernehmen? Dann bist Du bei uns genau richtig!</p>
+            <p>Zur Verstärkung unseres Verkaufsteams suchen wir zum
+                <strong>nächstmöglichen Zeitpunkt</strong> einen
+            </p>
+            <h3><strong><span style="color: var(--color-primary)">Elektromonteur / Elektriker als Servicetechniker
+                        (m/w/d)</span></strong></h3>
+            <p><strong>Zu Deinen Aufgaben zählt:</strong></p>
+            <ul>
+                <li>
+                    <p>Fehlerdiagnose, Reparatur und Wartung von Fleischereimaschinen, Gewerbespülmaschinen und
+                        thermischen Küchengeräten in der Gastronomie und der Gemeinschafts- und Schulverpflegung</p>
+                </li>
+                <li>
+                    <p>Installation und Montage von Neugeräten</p>
+                </li>
+                <li>
+                    <p>Geräteprüfungen nach DGUV-V3</p>
+                </li>
+            </ul>
+            <p><strong>Das solltest Du mitbringen:</strong></p>
+            <ul>
+                <li>
+                    <p>erfolgreich abgeschlossene Ausbildung in einem elektrotechnischen Beruf</p>
+                </li>
+                <li>
+                    <p>mehrjährige Berufserfahrung wünschenswert, aber nicht Bedingung</p>
+                </li>
+                <li>
+                    <p>Zuverlässigkeit, Teamfähigkeit, freundliches und kundenorientiertes Auftreten</p>
+                </li>
+                <li>
+                    <p>Selbstorganisation, Eigeninitiative und Mobilität</p>
+                </li>
+                <li>
+                    <p>Grundkenntnisse in MS-Office Anwendungen</p>
+                </li>
+                <li>
+                    <p>Führerschein Klasse B, vorteilhaft auch Klasse BE</p>
+                </li>
+            </ul>
+            <p><strong>Wir bieten Dir:</strong></p>
+            <ul>
+                <li>
+                    <p>attraktive Vergütung zzgl. Sonderzahlungen</p>
+                </li>
+                <li>
+                    <p>30 Tage Jahresurlaub</p>
+                </li>
+                <li>
+                    <p>Dienstfahrrad-Leasing</p>
+                </li>
+                <li>
+                    <p>umfangreiche Fortbildungsmöglichkeiten mit Kostenübernahme</p>
+                </li>
+                <li>
+                    <p>ein abwechslungsreiches und verantwortungsvolles Aufgabengebiet</p>
+                </li>
+                <li>
+                    <p>Gestellung von hochwertiger Arbeitsbekleidung, Werkzeug und modernen Service-Fahrzeugen</p>
+                </li>
+                <li>
+                    <p>Viele weitere Mitarbeitervorteile (z. B. betriebliche Altersvorsorge, Rabatte auf unsere
+                        Handelsprodukte, Corporate Benefits, Teamevents)</p>
+                </li>
+            </ul>
+            <p>Haben wir Dein Interesse geweckt?<br><br>Dann freuen wir uns auf Deine aussagekräftigen
+                Bewerbungsunterlagen mit Angabe Deiner Gehaltsvorstellung und Deines frühestmöglichen Eintrittstermins
+                per E-Mail an:</p>
+            <p><a target="_self" rel="noopener noreferrer nofollow"
+                    href="mailto:bewerbungen@fdbs.de">bewerbungen@fdbs.de</a></p>
+            <p>Für weitere Rückfragen steht Dir <strong>Jessica Ackermann </strong>telefonisch unter <a target="_self"
+                    rel="noopener noreferrer nofollow" href="tel:053121055100">0531-210 55 - 100</a> zur Verfügung.</p>
+        </div>
+    </SectionText>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    title: 'Elektromonteur / Elektriker als Servicetechniker',
+    description: 'Vollzeit, Werkstatt & Kundendienst, Industrie',
+})
+
+useSeoMeta({
+    title: 'Elektromonteur / Elektriker als Servicetechniker',
+    description: 'Vollzeit, Werkstatt & Kundendienst, Industrie',
+})
+</script>
+
+<style lang="sass" scoped></style>
