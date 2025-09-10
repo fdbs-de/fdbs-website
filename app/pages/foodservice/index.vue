1cfa4290bb13b0085343261b8e6ba868707aaeb8
Melina Kimmling <166021608+melina-at-fdbs@users.noreply.github.com>
2025-09-10 20:27:34 +0200

    add cover-cards on foodservice overview page

--- app/pages/foodservice/index.vue
+++ app/pages/foodservice/index.vue
@@ -1,8 +1,16 @@
 <template>
-    <SectionHero title="Unser Foodservice" image="/images/content/banner/foodservice_365702048_356901877_454625888.webp" />
-    <SectionText>
-        <!-- TODO: Add content -->
-    </SectionText>
+    <SectionHero title="Unser Foodservice"
+        image="/images/content/banner/foodservice_365702048_356901877_454625888.webp" />
+    <SectionGrid>
+        <Card :is="NuxtLink" cover to="/foodservice/mehrwegpflicht" name="Mehrwegpflicht" image="
+            /images/content/foodservice/mehrwegpflicht-cover.png" />
+
+        <Card :is="NuxtLink" cover to="/foodservice/rezepte" name="Rezepte"
+            image="/images/content/foodservice/recipe-cover.png" />
+
+        <Card :is="NuxtLink" cover to="/foodservice/tierhaltungskennzeichnung" name="Tierhaltungskennzeichnung"
+            image="/images/content/foodservice/tierhaltungskennzeichnung-cover.png" />
+    </SectionGrid>
 </template>
 
 <script lang="ts" setup>
@@ -10,6 +18,8 @@
         title: 'Unser Foodservice',
         description: '',
     })
+
+    const NuxtLink = defineNuxtLink({})
 </script>
 
 <style lang="sass" scoped></style>
