3ff09edc0b8b1bce0f334fedffeb53e1321f1f96
Melina Kimmling <166021608+melina-at-fdbs@users.noreply.github.com>
2025-09-22 13:46:42 +0200

    Add job offer "Schichtleitung Spätschicht"

---
+++ app/pages/karriere/schichtleitung-spaetschicht.vue
@@ -0,0 +1,128 @@
+<template>
+    <SectionHero title="Schichtleitung Spätschicht" image="/images/content/banner/karriere_513169496_482949832.webp" />
+    <SectionText>
+        <span>
+            Anstellungsart: <b>Vollzeit</b><br>
+            Tags: <b>Lager, Fleisch</b>
+        </span>
+        <HeDivider />
+        <div class="formatted-content">
+            <p>FDBS ist ein Braunschweiger Großhandelsunternehmen und versteht sich seit über 100 Jahren als Partner für
+                Fleischereien, Bäckereien, Gastronomen, und Hoteliers. Wir liefern Frischeprodukte,
+                High-End-Convenience, sowie nahezu alles an Bedarfsartikeln und inspirieren unsere Kunden mit immer
+                neuen Trends.</p>
+            <p style="text-align: start">Unser Ziel ist es, unsere Kunden mit qualitativ hochwertigen Lebensmitteln
+                schnell und zuverlässig zu beliefern. Neben der Qualität und Herkunft unserer Produkte spielen eine hohe
+                Kundenorientierung sowie kooperative Beziehungen zu unseren Geschäftspartnern für uns eine große Rolle.
+            </p>
+            <p style="text-align: start">Wir sind ein sich ständig weiterentwickelndes Unternehmen mit über 100
+                Mitarbeitern und bieten ergänzend zu unserem großen Handelssortiment einen einzigartigen Rundum-Service
+                in Kundendienst, Werkstatt und Marketing, der den Anforderungen unserer Kunden entspricht.</p>
+            <p style="text-align: start">Du hast Interesse an der Food Branche, suchst Abwechslung und bist bereit
+                Verantwortung zu übernehmen? Dann bist Du bei uns genau richtig!</p>
+            <p>Zur Verstärkung unseres Verkaufsteams suchen wir eine</p>
+            <h3><strong><span style="color: var(--color-primary)">Schichtleitung Spätschicht (m/w/d)</span></strong>
+            </h3>
+            <p><strong>Zu Deinen Aufgaben zählt:</strong></p>
+            <ul class="list-disc pl-6 mt-4">
+                <li>
+                    <p><span>Verantwortung für die Mitarbeiter in der Spätschicht</span></p>
+                </li>
+                <li>
+                    <p><span>Erstellen von Lieferdokumenten</span></p>
+                </li>
+                <li>
+                    <p><span>Ladungssicherung unseres Vollsortiments nach Vorgabe eines Tourenplans</span></p>
+                </li>
+                <li>
+                    <p><span>Kontrollieren der bereits kommissionierten Ware</span></p>
+                </li>
+                <li>
+                    <p><span>Verdichtung der Kommissionen in allen Waren- und Temperaturbereichen</span></p>
+                </li>
+                <li>
+                    <p><span>Beladung der LKWs am späten Abend und in der Nacht</span></p>
+                </li>
+                <li>
+                    <p><span>Du arbeitest in der Spätschicht</span></p>
+                    <p></p>
+                </li>
+            </ul>
+            <p><strong>Das solltest Du mitbringen:</strong></p>
+            <ul class="list-disc pl-6 mt-4">
+                <li>
+                    <p><span>Erste Erfahrungen in der Logistik oder im Großhandel</span></p>
+                </li>
+                <li>
+                    <p><span>Körperliche Belastbarkeit, Zuverlässigkeit und Teamgeist</span></p>
+                </li>
+                <li>
+                    <p><span>Hohe Einsatzbereitschaft und Flexibilität</span></p>
+                </li>
+                <li>
+                    <p><span>Spaß an körperlicher Arbeit</span></p>
+                </li>
+                <li>
+                    <p><span>Staplerschein wünschenswert, aber nicht Bedingung</span></p>
+                </li>
+                <li>
+                    <p><span>Selbstständige und gewissenhafte Arbeitsweise</span></p>
+                </li>
+                <li>
+                    <p><span>Bereitschaft zur Nachtarbeit</span></p>
+                    <p></p>
+                </li>
+            </ul>
+            <p><strong>Wir bieten Dir:</strong></p>
+            <ul class="list-disc pl-6 mt-4">
+                <li>
+                    <p>attraktive Vergütung zzgl. Sonderzahlungen</p>
+                </li>
+                <li>
+                    <p>steuerfreie Zuschläge für Sonntags-, Feiertags- und Nachtarbeit</p>
+                </li>
+                <li>
+                    <p>kurze Probezeit von 3 Monaten</p>
+                </li>
+                <li>
+                    <p>30 Tage Jahresurlaub</p>
+                </li>
+                <li>
+                    <p>unbefristeter Arbeitsvertrag</p>
+                </li>
+                <li>
+                    <p>umfangreiche Fortbildungsmöglichkeiten mit Kostenübernahme</p>
+                </li>
+                <li>
+                    <p>Gestellung von Arbeitsbekleidung</p>
+                </li>
+                <li>
+                    <p>Viele weitere Mitarbeitervorteile (z. B. betriebliche Altersvorsorge, Rabatte auf unsere
+                        Handelsprodukte, Corporate Benefits, Teamevents)</p>
+                </li>
+            </ul>
+            <p><strong>Arbeitszeiten:</strong></p>
+            <p>
+                Montag bis Donnerstag: 16:00 Uhr bis ca. 00:45 Uhr <br>Freitag: 16:00 Uhr bis 23:45 Uhr
+            </p>
+            <p>Haben wir Dein Interesse geweckt?<br><br>Dann freuen wir uns auf Deine aussagekräftigen
+                Bewerbungsunterlagen mit Angabe Deiner Gehaltsvorstellung und Deines frühestmöglichen Eintrittstermins
+                per E-Mail an:</p>
+            <p><a target="_self" rel="noopener noreferrer nofollow"
+                    href="mailto:bewerbungen@fdbs.de">bewerbungen@fdbs.de</a></p>
+            <p>Für weitere Rückfragen steht Dir <strong>Jessica Ackermann </strong>telefonisch unter <a target="_self"
+                    rel="noopener noreferrer nofollow" href="tel:053121055100">0531-210 55 - 100</a> zur Verfügung.</p>
+        </div>
+    </SectionText>
+</template>
+<script lang="ts" setup>
+definePageMeta({
+    title: 'Schichtleitung Spätschicht',
+    description: 'Vollzeit, Lager, Fleisch',
+})
+useSeoMeta({
+    title: 'Schichtleitung Spätschicht',
+    description: 'Vollzeit, Lager, Fleisch',
+})
+</script>
+<style lang="sass" scoped></style>
