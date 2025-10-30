08e10b74ab9c0ce69c067d1268374f11d6ba6396
Alyx Freuwört <alyx@freuwoert.com>
2025-10-30 18:31:09 +0100

    Edit angebote layout

--- app/components/app/AppOfferExplorer.vue
+++ app/components/app/AppOfferExplorer.vue
@@ -1,21 +1,34 @@
 <template>
-    <IodInput class="!h-10 !rounded-full w-full md:w-auto" placeholder="Suchen" clearable v-model="search">
+    <!-- <IodInput class="!h-10 !rounded-full w-full md:w-auto" placeholder="Suchen" clearable v-model="search">
         <template #right>
             <IodIconButton type="button" class="!w-12" icon="refresh" size="s" corner="pill" variant="text" @click="fetch" v-tooltip="'Aktualisieren'"/>
         </template>
-    </IodInput>
-
-    <IodLoader type="bar" v-show="loading"/>
+    </IodInput> 
+    <IodLoader type="bar" v-show="loading"/> -->
 
     <h3>Aktuelle Angebote</h3>
     <div class="featured-grid" v-if="featuredItems.length ">
-        <MediaItem v-for="item in featuredItems" :item="item" />
+        <a class="offer-item" v-for="item in featuredItems" :href="item.cdn_path || '#'" target="_blank" rel="noopener">
+            <img :src="item.thumbnail || ''" :alt="item.name" />
+            <span v-tooltip="item.name">{{ item.name }}</span>
+        </a>
     </div>
+    <IodAlert v-else as="placeholder" class="h-64 border">
+        Keine aktuellen Angebote verfügbar.
+    </IodAlert>
 
-    <h3>Alle Angebote</h3>
+    <slot name="banner" />
+
+    <h3 class="mt-16">Alle Angebote</h3>
     <div class="content-grid" v-if="items.length ">
-        <MediaItem v-for="item in items" :item="item" />
+        <a class="offer-item" v-for="item in items" :href="item.cdn_path || '#'" target="_blank" rel="noopener">
+            <img :src="item.thumbnail || ''" :alt="item.name" />
+            <span v-tooltip="item.name">{{ item.name }}</span>
+        </a>
     </div>
+    <IodAlert v-else as="placeholder" class="h-64 border">
+        Keine Angebote verfügbar.
+    </IodAlert>
 </template>
 
 <script lang="ts" setup>
@@ -79,6 +92,63 @@
 
     .content-grid
         display: grid
-        grid-template-columns: repeat(auto-fit, minmax(150px, 250px))
+        grid-template-columns: repeat(5, 1fr)
         gap: 1rem
+
+    .offer-item
+        position: relative
+        display: flex
+        flex-direction: column
+        align-items: center
+        text-align: center
+        color: var(--color-text)
+        border-radius: .5rem
+        overflow: hidden
+        transition: all 100ms
+
+        > img
+            position: relative
+            z-index: 1
+            width: 100%
+            max-height: 300px
+            border-radius: .5rem
+            border: 1px solid var(--color-border)
+            object-fit: contain
+            background: var(--color-background-soft)
+            transition: all 100ms
+
+        > span
+            position: relative
+            z-index: 1
+            margin-block: .5rem
+            font-weight: 600
+
+        &::after
+            content: ''
+            position: absolute
+            inset: 0
+            z-index: 0
+            opacity: 0
+            background: var(--color-primary)
+
+        &:hover,
+        &:focus
+            color: var(--color-primary)
+
+            &::after
+                opacity: .1
+
+            img
+                background: currentColor
+                border: 2px solid currentColor
+
+        &:focus
+            outline: 2px solid var(--color-primary)
+
+    @media (max-width: 768px)
+        .featured-grid
+            grid-template-columns: 1fr
+
+        .content-grid
+            grid-template-columns: repeat(2, 1fr)
 </style>
--- app/pages/angebote.vue
+++ app/pages/angebote.vue
@@ -1,7 +1,20 @@
 <template>
-    <SectionHero title="Unsere aktuellen Angebote" image="/images/content/banner/angebote_526077708.webp" />
+    <SectionHero title="Unsere Angebote" image="/images/content/banner/angebote_526077708.webp" />
     <SectionText size="page">
-        <AppOfferExplorer content="domain/angebote/ohne_preise" featured="domain/angebote/ohne_preise/aktuell"/>
+        <AppOfferExplorer content="domain/angebote/ohne_preise" featured="domain/angebote/ohne_preise/aktuell">
+            <template #banner>
+                <div class="online-shop-card mt-16">
+                    <div class="wrapper">
+                        <h3>Unser neuer Online-Shop</h3>
+                        <p>
+                            Entdecken Sie unsere große Auswahl an frischen und hochwertigen Lebensmitteln - einfach, schnell und zuverlässig bestellt.
+                        </p>
+                        <NuxtLink to="/foodservice/online-shop">Jetzt den neuen Online-Shop testen und bequem einkaufen!</NuxtLink>
+                    </div>
+                    <img src="https://fdbs.de/images/content/online-shop/online-shop-cover.jpg" alt="Hände halten ein Handy mit dem Online-Shop des FDBS"/>
+                </div>
+            </template>
+        </AppOfferExplorer>
     </SectionText>
 </template>
 
@@ -12,4 +25,60 @@
     })
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+    .online-shop-card
+        background-color: var(--color-background-soft)
+        color: var(--color-text-soft)
+        border-radius: 1rem
+        padding: .5rem
+        position: relative
+        overflow: hidden
+        display: grid
+        align-items: center
+        grid-template-columns: 1fr 300px
+        grid-template-areas: "content image"
+
+        .wrapper
+            grid-area: content
+            padding: 1rem
+
+        img
+            height: 100%
+            object-fit: cover
+            border-radius: .5rem
+            mask-image: linear-gradient(to right, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 50%)
+
+        h3
+            color: var(--color-text)
+            font-size: clamp(1.75rem, 5vw, 2.5rem)
+            margin-block: 0
+            position: relative
+
+        a
+            color: var(--color-primary)
+            text-decoration: underline
+            font-weight: 600
+            border-radius: 3px
+
+            &:hover
+                color: var(--color-secondary)
+
+            &:focus
+                outline: 2px solid var(--color-secondary)
+
+        p
+            margin-bottom: .4rem
+            position: relative
+            max-width: 600px
+
+    @media (max-width: 768px)
+
+        .online-shop-card
+            grid-template-columns: 1fr
+            grid-template-areas: "image" "content"
+
+            img
+                width: 100%
+                height: auto
+                mask-image: none
+</style>
